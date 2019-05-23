9f3b8d0cc93aff8a17d7f765584b8881a30030ad
meme <meme.lau@mail.com>
2019-05-23 17:16:57 +0800

    init

--- src/main/java/ml/memelau/xxl/job/XxlJobLoggerAppender.java
+++ src/main/java/ml/memelau/xxl/job/XxlJobLoggerAppender.java
@@ -4,7 +4,6 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
 import com.xxl.job.core.log.XxlJobFileAppender;
 import com.xxl.job.core.log.XxlJobLogger;
-import lombok.SneakyThrows;
 import org.joor.Reflect;
 
 import java.util.Optional;
@@ -23,7 +22,6 @@ public class XxlJobLoggerAppender extends AppenderBase<ILoggingEvent> {
                 .ifPresent(__ -> doLog(eventObject));
     }
 
-    @SneakyThrows
     private void doLog(ILoggingEvent event) {
         Reflect.on(XxlJobLogger.class)
                .call("logDetail",
