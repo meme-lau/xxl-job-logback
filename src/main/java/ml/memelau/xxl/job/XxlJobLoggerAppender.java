2cc4955e17fd66a6c52c52521ad0dcc7661fb191
meme <meme.lau@mail.com>
2019-05-23 15:30:42 +0800

    init

---
+++ src/main/java/ml/memelau/xxl/job/XxlJobLoggerAppender.java
@@ -0,0 +1,34 @@
+package ml.memelau.xxl.job;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.AppenderBase;
+import com.xxl.job.core.log.XxlJobFileAppender;
+import com.xxl.job.core.log.XxlJobLogger;
+import lombok.SneakyThrows;
+
+import java.lang.reflect.Method;
+import java.util.Optional;
+
+
+/**
+ * XxlJobLoggerAppender
+ *
+ * @author meme
+ * @since 2019-05-23 10:50
+ */
+public class XxlJobLoggerAppender extends AppenderBase<ILoggingEvent> {
+
+    protected void append(ILoggingEvent eventObject) {
+        Optional.ofNullable(XxlJobFileAppender.contextHolder.get())
+                .ifPresent(__ -> doLog(eventObject));
+    }
+
+    @SneakyThrows
+    private void doLog(ILoggingEvent event) {
+        Method logDetail = XxlJobLogger.class.getDeclaredMethod("logDetail", StackTraceElement.class, String.class);
+        logDetail.setAccessible(true);
+        logDetail.invoke(null, event.getCallerData()[0],
+                String.format("[%s] [%s] %s", event.getLoggerName(), event.getLevel(), event.getFormattedMessage()));
+    }
+
+}
